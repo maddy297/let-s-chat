ea7b7206d5bad4fbf633cf36eee00eb498584f59
maddy297 <81658887+maddy297@users.noreply.github.com>
2021-08-24 16:48:17 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+      apiKey: "AIzaSyDnRNlSLs-R9ONwLKIYqxfYI-oUpcDJeVs",
+      authDomain: "kwitter-app-61e52.firebaseapp.com",
+      databaseURL: "https://kwitter-app-61e52-default-rtdb.firebaseio.com",
+      projectId: "kwitter-app-61e52",
+      storageBucket: "kwitter-app-61e52.appspot.com",
+      messagingSenderId: "252252245221",
+      appId: "1:252252245221:web:cabd1771a6ab73e78b580c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
